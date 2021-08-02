922f48e24401f092457a25937686eec95961651b
James Long <longster@gmail.com>
2021-08-02 10:51:12 -0400

    Add reset_filesystem

--- src/api.js
+++ src/api.js
@@ -1246,4 +1246,9 @@ Module["onRuntimeInitialized"] = function onRuntimeInitialized() {
       let fileInfo = filesInfo.find(f => f[1] === path);
       sqliteFiles.delete(fileInfo[0])
     }
+
+    Module["reset_filesystem"] = () => {
+      FS.root = null;
+      FS.staticInit();
+    }
 };
